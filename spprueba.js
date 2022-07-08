cb7b4ee923c97d973d76d687f8306a8755b47d93
IpodCraft <57160583+IpodCraft@users.noreply.github.com>
2022-07-08 18:41:36 -0500

    final pt.1

---
+++ spprueba.js
@@ -0,0 +1,3 @@
+ document.getElementsByClassName("ac1").innerHTML = ("AdrianCraft");
+ document.getElementsByClassName("ac2").innerHTML = ("AdrianCraft2");
+ document.getElementsByClassName("ac3").innerHTML = ("AdrianCraft3");
