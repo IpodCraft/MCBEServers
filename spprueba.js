33e45ce80edbfe0239512e102f506e06c2a1cbef
IpodCraft <57160583+IpodCraft@users.noreply.github.com>
2022-07-08 18:47:25 -0500

    innecesary

--- spprueba.js
+++
@@ -1,3 +0,0 @@
- document.getElementsByClassName("ac1").innerHTML = ("AdrianCraft");
- document.getElementsByClassName("ac2").innerHTML = ("AdrianCraft2");
- document.getElementsByClassName("ac3").innerHTML = ("AdrianCraft3");
