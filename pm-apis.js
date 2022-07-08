1fadb03a2211ba5eec4d5b1384c614a81639e52c
IpodCraft <57160583+IpodCraft@users.noreply.github.com>
2022-07-08 00:12:43 -0500

    upload

---
+++ pm-apis.js
@@ -0,0 +1,13 @@
+// POCKETMINE-MP SERVER //
+document.getElementById("api4").innerHTML = "4.5.2";
+document.getElementById("api5").innerHTML = "5.X.X";
+document.getElementById("api6").innerHTML = "6.X.X";
+document.getElementById("api7").innerHTML = "7.X.X";
+
+
+
+// DEDICATED BEDROCK SERVER //
+document.getElementById("v19").innerHTML = "1.19.2";
+document.getElementById("v20").innerHTML = "1.2X.X";
+document.getElementById("v21").innerHTML = "1.3X.X";
+document.getElementById("v22").innerHTML = "1.4X.X";
